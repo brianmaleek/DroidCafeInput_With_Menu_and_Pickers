a87ed73636141bcdb1418d030412187ab55d481f
brianmaleek <maleekmalik@gmail.com>
2020-09-24 02:20:33 +0300

    App Reference: Input Controls 1. Added Text box views (single and multiline). 2. Added Radio buttons with toast message 3. Added onClick listeners for the radio buttons.
    cont
    4, Added an array for the drop down spinner
    5. Created an array adapter to populate the spinner.
    6. Used the spinner android layout resources simple_spinner_item and simple_spinner_dropdown_item).

--- app/src/main/java/com/brma/droidcafe/OrderActivity.java
+++ app/src/main/java/com/brma/droidcafe/OrderActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +20,24 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
 
         Intent intent = getIntent();
-        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(message);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
     }
 
     public void onRadioButtonClicked(View view) {
@@ -43,5 +62,17 @@ public class OrderActivity extends AppCompatActivity {
 
     public void displayToast (String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String spinnerLabel = adapterView.getItemAtPosition (position).toString();
+        displayToast(spinnerLabel);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
