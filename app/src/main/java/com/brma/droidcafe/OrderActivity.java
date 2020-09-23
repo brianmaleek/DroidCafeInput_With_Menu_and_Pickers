2a8a6e015225b6e1f2e288c5cb2638fd46e533ee
brianmaleek <maleekmalik@gmail.com>
2020-09-23 10:56:52 +0300

    App Reference: 1. Added Text box views (single and multiline). 2. Added Radio buttons with toast message 3. Added onClick listeners for the radio buttons.

--- app/src/main/java/com/brma/droidcafe/OrderActivity.java
+++ app/src/main/java/com/brma/droidcafe/OrderActivity.java
@@ -3,7 +3,10 @@ package com.brma.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -16,5 +19,29 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.sameday));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.nextday));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void displayToast (String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
