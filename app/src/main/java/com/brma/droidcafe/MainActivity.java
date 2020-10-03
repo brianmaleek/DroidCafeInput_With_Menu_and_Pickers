9ff0093ba6e09f37f4596565ae483e8efe734231
brianmaleek <maleekmalik@gmail.com>
2020-10-03 06:04:37 +0300

    App Reference: Input Controls 1. Added items to the options menu. This option menu items are:         Order: Navigate to OrderActivity to see the dessert order.         Status: Check the status of an order.         Favorites: Show favorite desserts.         Contact: Contact the cafe. Because you don't need the existing Settings item, you will change Settings to Contact.

--- app/src/main/java/com/brma/droidcafe/MainActivity.java
+++ app/src/main/java/com/brma/droidcafe/MainActivity.java
@@ -3,7 +3,6 @@ package com.brma.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -52,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_order) {
             return true;
         }
 
